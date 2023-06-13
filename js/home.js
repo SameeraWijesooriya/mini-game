305497626d35d6f82837d2e3d7fcac9cf2a09ade
sameera <sameeram.wijesooriya95@gmail.com>
2023-06-13 14:20:55 +0530

    Start page create

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-document.body.style.backgroundImage=`url('img/bg2.png')`;
+document.body.style.backgroundImage=`url('img/'imgBG.jpg)`;
 document.body.style.backgroundSize='cover';
 function exit() {
     window.close()
